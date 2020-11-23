f78be7a2caab5c8ed7205f95a6ecb7e4ef0bee77
shinji ichien <shinji11088011@gmail.com>
2020-11-23 15:59:50 +0900

    add use what changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+/**
+ * Uncomment the hooks below uwc-debug comment to see the the output
+ * in console, this hooks works for useEffect, useCallback, useMemo which almost cover most of the the
+ * hooks that we use on a daily basis
+ */
+const App = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  //uwc-debug
+  useEffect(() => {
+    // do something
+  }, [a, b]);
+
+  // uwc-debug
+  const v = useMemo(() => {
+    return a + 1;
+  }, [a]);
+
+  // uwc-debug
+  const callback = useCallback(() => {
+    console.log(b);
+    return "some callback";
+  }, [b]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Start editing to see some magic happen!</h2>
+      <button
+        onClick={() => {
+          setA(c => c + 1);
+        }}
+      >
+        Change A
+      </button>
+      <button
+        onClick={() => {
+          setB(c => c + 1);
+        }}
+      >
+        Change B
+      </button>
+      <h2>value:a</h2>
+      <div>{a}</div>
+      <h2>value:b</h2>
+      <div>{b}</div>
     </div>
   );
-}
+};
 
 export default App;
+
